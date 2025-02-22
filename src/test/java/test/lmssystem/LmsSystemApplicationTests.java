814766345c62116dce4ec4045e30609acf7eae04
HUIJAEKO <xrhgmlwox@naver.com>
2025-02-22 13:43:50 +0900

    First commit

---
+++ src/test/java/test/lmssystem/LmsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package test.lmssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
